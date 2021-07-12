f616d75773aa9c15bccad58612ed722b6e2b1765
gentry <gma23@txstate.edu>
2021-07-12 15:51:05 -0400

    outlined the big file

--- src/process_all_data.py
+++ src/process_all_data.py
@@ -2,3 +2,34 @@
 #Organization: Texas University
 #Data: 12 July, 2021
 #Read the 6 datasets, write in nice format, and then write noisy label settings
+#This is going to be a big one_hot_encode
+
+#6 Datasets: 2 synthetic, 2 HAR, 2 BioSignal
+#Each dataset will have 7 label sets:
+#   -clean
+#   -low noise NCAR
+#   -high noise NCAR
+#   -low noise NAR
+#   -high noise NAR
+#   -low noise NNAR
+#   -high noise NNAR
+
+
+
+#Create Synthetic Set 1
+#Create label sets for SS1
+
+#Create Synthetic Set 2
+#Create label sets for SS2
+
+#Use Lee's files to get HAR Set 1
+#Create label sets for HAR1
+
+#Process UCI HAR inertial signals into a good file
+#Create label sets for HAR2
+
+#Process Sleep Apnea set into BioSignal Set 1
+#Create label sets for BS1
+
+#Process PD Gait set into BioSignal Set 2
+#Create label sets for BS2
