8ad9970c0a4bd1425c824372efc1a299ffb3c5c3
gentry <gma23@txstate.edu>
2021-07-13 15:17:02 -0400

    added 2nd synthetic set to processor

--- src/process_all_data.py
+++ src/process_all_data.py
@@ -25,19 +25,28 @@ PATH = 'src/data/processed_datasets/'
 if(__name__ == "__main__"):
 
     #Create Synthetic Set 1
-    print("### Preparing Dataset: SS1 ###")
-    ss1_attributes, ss1_labels_clean = generate_pattern_data_as_dataframe(length=150, numSamples=10000, numClasses=2, percentError=0)
-    ss1_attributes = np.reshape(np.array(ss1_attributes['x']),(10000, 150))
-    np.savetxt(PATH + 'ss1_attributes.csv', ss1_attributes,  delimiter=',')
-    np.savetxt(PATH + 'ss1_labels_clean.csv', ss1_labels_clean, delimiter=',', fmt='%d')
+    print("##### Preparing Dataset: SS1 #####")
+    attributes, labels_clean = generate_pattern_data_as_dataframe(length=150, numSamples=10000, numClasses=2, percentError=0)
+    attributes = np.reshape(np.array(attributes['x']),(10000, 150))
+    np.savetxt(PATH + 'ss1_attributes.csv', attributes,  delimiter=',')
+    np.savetxt(PATH + 'ss1_labels_clean.csv', labels_clean, delimiter=',', fmt='%d')
 
     #Create label sets for SS1
-    add_ncar(ss1_labels_clean, PATH + 'ss1_labels', 2)
-    add_nar(ss1_labels_clean, PATH + 'ss1_labels', 2)
-    add_nnar(ss1_attributes, ss1_labels_clean, PATH + 'ss1_labels', 2)
+    add_ncar(labels_clean, PATH + 'ss1_labels', 2)
+    add_nar(labels_clean, PATH + 'ss1_labels', 2)
+    add_nnar(attributes, labels_clean, PATH + 'ss1_labels', 2)
 
     #Create Synthetic Set 2
+    print("##### Preparing Dataset: SS2 #####")
+    attributes, labels_clean = generate_pattern_data_as_dataframe(length=150, numSamples=30000, numClasses=5, percentError=0)
+    attributes = np.reshape(np.array(attributes['x']),(30000, 150))
+    np.savetxt(PATH + 'ss2_attributes.csv', attributes,  delimiter=',')
+    np.savetxt(PATH + 'ss2_labels_clean.csv', labels_clean, delimiter=',', fmt='%d')
+
     #Create label sets for SS2
+    add_ncar(labels_clean, PATH + 'ss2_labels', 5)
+    add_nar(labels_clean, PATH + 'ss2_labels', 5)
+    add_nnar(attributes, labels_clean, PATH + 'ss2_labels', 5)
 
     #Use Lee's files to get HAR Set 1
     #Create label sets for HAR1
