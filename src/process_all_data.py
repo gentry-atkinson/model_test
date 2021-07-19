6197b5be25e47749abe8e107194f791db745a9ef
gentry <gma23@txstate.edu>
2021-07-19 16:18:27 -0400

    bs test set done

--- src/process_all_data.py
+++ src/process_all_data.py
@@ -19,6 +19,7 @@ from utils.add_ncar import add_ncar
 from utils.add_nar import add_nar
 from utils.add_nnar import add_nnar
 from data.e4_wristband_Nov2019.e4_get_x_y_sub import get_X_y_sub
+from data.e4_wristband_Nov2019.e4_load_dataset import e4_load_dataset
 from import_datasets import get_uci_data, get_uci_test
 import numpy as np
 import os
@@ -28,8 +29,8 @@ from scipy.signal import resample
 PATH = 'src/data/processed_datasets/'
 
 RUN_SS = False
-RUN_HAR = True
-RUN_BS = False
+RUN_HAR = False
+RUN_BS = True
 
 if(__name__ == "__main__"):
 
@@ -68,30 +69,44 @@ if(__name__ == "__main__"):
     if RUN_HAR:
         print("##### Preparing Dataset: HAR1 #####")
         #Use Lee's files to get HAR Set 1
-        attributes, labels_clean, sub, xyinfo = get_X_y_sub(working_dir='src/temp', zip_flist=[
-            '1574621345_A01F11.zip', '1574622389_A01F11.zip',
-            '1574624998_A01F11.zip', '1574625540_A01F11.zip'], time_steps=150,
-            step=50)
+        #attributes, labels_clean, sub, xyinfo = get_X_y_sub(working_dir='src/temp', zip_flist=[
+        #    '1574621345_A01F11.zip', '1574622389_A01F11.zip',
+        #    '1574624998_A01F11.zip', '1574625540_A01F11.zip'], time_steps=150,
+        #    step=50)
+        attributes, labels_clean, att_test, lab_test = e4_load_dataset()
         attributes = np.array(attributes)
-        label_dic = {
-            'Downstairs':0,
-            'Jogging':1,
-            'Not_Labeled':2,
-            'Sitting':3,
-            'Standing':4,
-            'Upstairs':5,
-            'Walking':6,
-        }
-        labels_clean = np.array([label_dic[i[0]] for i in labels_clean])
+        att_test = np.array(att_test)
+        # label_dic = {
+        #     'Downstairs':0,
+        #     'Jogging':1,
+        #     'Not_Labeled':2,
+        #     'Sitting':3,
+        #     'Standing':4,
+        #     'Upstairs':5,
+        #     'Walking':6,
+        # }
+        # labels_clean = np.array([label_dic[i[0]] for i in labels_clean])
+        # lab_test = np.array([label_dic[i[0]] for i in lab_test])
+        labels_clean = np.argmax(labels_clean, axis=-1)
+        lab_test = np.argmax(lab_test, axis=-1)
         num_instances = attributes.shape[0]
         num_samples = attributes.shape[1]
         num_channels = attributes.shape[2]
+        print('Number of e4 instances: ', len(attributes))
+        print('Number of e4 labels: ', len(labels_clean))
+        print('Shape of  e4 attributes: ', np.array(attributes).shape)
         attributes = np.reshape(attributes,(num_instances*num_channels, num_samples))
-        print('Number of instances: ', len(attributes))
-        print('Number of labels: ', len(labels_clean))
-        print('Shape of attributes: ', np.array(attributes).shape)
-        np.savetxt(PATH + 'har1_attributes.csv', attributes,  delimiter=',')
+        num_instances = att_test.shape[0]
+        num_samples = att_test.shape[1]
+        num_channels = att_test.shape[2]
+        print('Number of test instances: ', len(att_test))
+        print('Number of test labels: ', len(lab_test))
+        print('Shape of test attributes: ', np.array(attributes).shape)
+        att_test = np.reshape(att_test,(num_instances*num_channels, num_samples))
+        np.savetxt(PATH + 'har1_attributes_train.csv', attributes,  delimiter=',')
         np.savetxt(PATH + 'har1_labels_clean.csv', labels_clean, delimiter=',', fmt='%d')
+        np.savetxt(PATH + 'har1_attributes_test.csv', att_test,  delimiter=',')
+        np.savetxt(PATH + 'har1_labels_test.csv', lab_test, delimiter=',', fmt='%d')
 
         #Create label sets for HAR1
         add_ncar(labels_clean, PATH + 'har1_labels', 7)
@@ -123,9 +138,9 @@ if(__name__ == "__main__"):
         #falls apart
         attributes = []
         labels_clean = []
-        os.system('rm {}'.format(PATH + 'bs1_attributes.csv'))
+        os.system('rm {}'.format(PATH + 'bs1_attributes_train.csv'))
         os.system('rm {}'.format(PATH + 'bs1_labels_clean.csv'))
-        att_file = open(PATH + 'bs1_attributes.csv', 'a+')
+        att_file = open(PATH + 'bs1_attributes_train.csv', 'a+')
         lab_file = open(PATH + 'bs1_labels_clean.csv', 'a+')
         with open('src/data/apnea-ecg-database-1.0.0/list') as file_list:
             if not os.path.isdir('src/data/apnea-ecg-database-1.0.0/temp'):
@@ -152,17 +167,53 @@ if(__name__ == "__main__"):
                                 lab_file.write('{}\n'.format(0 if g[2] == 'N' else 1))
                                 #attributes = np.append(attributes, att[int(g[1]):int(g[1])+5999])
                                 labels_clean = np.append(labels_clean, [0 if g[2] == 'N' else 1])
-        attributes = np.array(attributes)
         labels_clean = np.array(labels_clean)
         att_file.close()
+        lab_file.close()
 
         #Create label sets for BS1
         add_ncar(labels_clean, PATH + 'bs1_labels', 2)
         add_nar(labels_clean, PATH + 'bs1_labels', 2)
-        add_nnar([], labels_clean, PATH + 'bs1_labels', 2, att_file=PATH+'bs1_attributes.csv')
+        add_nnar([], labels_clean, PATH + 'bs1_labels', 2, att_file=PATH+'bs1_attributes_train.csv')
+
+        #Create Test Set
+        attributes = []
+        labels_clean = []
+        os.system('rm {}'.format(PATH + 'bs1_attributes_test.csv'))
+        os.system('rm {}'.format(PATH + 'bs1_labels_test.csv'))
+        att_file = open(PATH + 'bs1_attributes_test.csv', 'a+')
+        lab_file = open(PATH + 'bs1_labels_test.csv', 'a+')
+        with open('src/data/apnea-ecg-database-1.0.0/list') as file_list:
+            if not os.path.isdir('src/data/apnea-ecg-database-1.0.0/temp'):
+                os.system('mkdir src/data/apnea-ecg-database-1.0.0/temp')
+            try:
+                system.os('rdann -h')
+            except:
+                print("Must install rdann from Physionet")
+            for f in file_list:
+                f = f.strip()
+                if f != '\n' and f[0]=='x':
+                    os.system('rdann -r src/data/apnea-ecg-database-1.0.0/{0} -a apn -f 0 > src/data/apnea-ecg-database-1.0.0/temp/{0}.txt'.format(f))
+                    att, ident = wfdb.rdsamp('src/data/apnea-ecg-database-1.0.0/{0}'.format(f))
+                    SIG_LEN = len(att)
+                    print("Read: ", SIG_LEN, " values from ", f)
+                    att = np.reshape(att, (SIG_LEN,1))
+                    with open('src/data/apnea-ecg-database-1.0.0/temp/{}.txt'.format(f)) as g_list:
+                        for g in g_list:
+                            g = g.strip().split(' ')
+                            g = [i for i in g if i != '']
+                            #att, ident = wfdb.rdsamp('src/data/apnea-ecg-database-1.0.0/{0}'.format(f), sampfrom=int(g[1]), sampto=int(g[1])+5999, warn_empty=True)
+                            if (int(g[1])+6000) < SIG_LEN:
+                                att_file.write('{}\n'.format(','.join([str(i[0]) for i in resample(att[int(g[1]):int(g[1])+6000], 600)])))
+                                lab_file.write('{}\n'.format(0 if g[2] == 'N' else 1))
+                                #attributes = np.append(attributes, att[int(g[1]):int(g[1])+5999])
+                                labels_clean = np.append(labels_clean, [0 if g[2] == 'N' else 1])
+        att_file.close()
+        lab_file.close()
 
         #Process PD Gait set into BioSignal Set 2
         #window the data to 5 second segments
+        print("##### Preparing Dataset: BS2 #####")
         attributes = []
         labels_clean = []
         os.system('rm {}'.format(PATH + 'bs2_attributes.csv'))
