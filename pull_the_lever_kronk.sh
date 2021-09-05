cdc79c93e05eff4e95366224e659f6d075e42a02
gentry <gma23@txstate.edu>
2021-09-04 21:27:09 -0500

    made the script to run the whole thing

---
+++ pull_the_lever_kronk.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#Author: Gentry Atkinson
+#Organization: Texas University
+#Data: 04 September, 2021
+#Run every script in the model test
+
+echo "Processing Datasets"
+python3 src/process_all_data.py > logs/preprocess_log.txt
+
+echo "Making Visualizations"
+python3 src/visualizer.py > logs/visualizer_log.txt
+
+echo "CNN Tests"
+python3 src/CNN_tests.py > logs/CNN_log.txt
+
+echo "LSTM Tests"
+python3 src/CNN_tests.py > logs/LSTM_log.txt
+
+echo "SVM Tests"
+python3 src/CNN_tests.py > logs/SVM_log.txt
+
+echo "NB Tests"
+python3 src/CNN_tests.py > logs/NB_log.txt
