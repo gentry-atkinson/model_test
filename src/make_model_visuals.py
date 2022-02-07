fb783c246b66ff1112861e13f28700b970607b3d
gentry <gma23@txstate.edu>
2022-02-07 08:16:27 -0600

    model pics look decent

--- src/make_model_visuals.py
+++ src/make_model_visuals.py
@@ -15,20 +15,26 @@
 # }
 
 import visualkeras
-from tensorflow.keras.models import Sequential
-from tensorflow.keras import layers
-import tensorflow.keras.metrics as met
+from tensorflow.python.keras.models import Sequential, Model
+from  tensorflow.python.keras.engine.functional import ModuleWrapper
+from tensorflow.keras.layers import BatchNormalization, LayerNormalization
+from tensorflow.python.keras import layers
+import tensorflow.python.keras.metrics as met
 from utils.gen_ts_data import generate_pattern_data_as_array
 from model_config import loadDic
 import numpy as np
 from random import randint
+# from tensorflow.keras.utils import to_categorical
+from PIL import ImageFont
+from collections import defaultdict
+import matplotlib
 
 def build_cnn(X, num_classes, set, num_channels=1, opt='SGD', loss='mean_squared_error'):
     config_dic = loadDic('CNN')
     print("Input Shape: ", X.shape)
     model = Sequential([
         layers.Input(shape=X[0].shape),
-        layers.BatchNormalization(),
+        BatchNormalization(),
         layers.Conv1D(filters=config_dic[set]['l1_numFilters']*1, kernel_size=config_dic[set]['l1_kernelSize'], padding='causal', activation='relu', groups=1),
         layers.Conv1D(filters=config_dic[set]['l1_numFilters']*1, kernel_size=config_dic[set]['l1_kernelSize'], padding='causal', activation='relu', groups=1),
         layers.MaxPooling1D(pool_size=(config_dic[set]['l1_maxPoolSize']*1), data_format='channels_first'),
@@ -43,20 +49,103 @@ def build_cnn(X, num_classes, set, num_channels=1, opt='SGD', loss='mean_squared
     model.summary()
     return model
 
+def build_lstm(X, num_classes, set, opt='SGD', loss='mean_squared_error'):
+    config_dic = loadDic('LSTM')
+    print("Input Shape: ", X.shape)
+    model = Sequential([
+        layers.Input(shape=X[0].shape),
+        BatchNormalization(),
+        layers.LSTM(config_dic[set]['lstm_units'], recurrent_dropout=config_dic[set]['dropout'], activation='relu'),
+        layers.Dropout(config_dic[set]['dropout']),
+        layers.Dense(config_dic[set]['hidden_dense_size'], activation='relu'),
+        layers.Dense(num_classes, activation='softmax')
+    ])
+    model.compile(optimizer=opt, loss=loss, metrics=[met.CategoricalAccuracy()])
+    model.summary()
+    return model
+
+def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
+    """
+    TransformerEncoder for time-series encoder, conforming to the
+    transformer architecture from Attention Is All You Need (Vaswani 2017)
+    https://arxiv.org/abs/1706.03762
+    @param (int) num_heads: Number of Attention Heads
+    @param (int) head_size : Head Size
+    @param (int) ff_dim: Feed Forward Dimension
+    @param (float) dropout : Dropout (between 0 and .99)
+    
+    Return: transformer encoder
+    """
+
+    # Normalization and Attention
+    print(type(inputs))
+    x = BatchNormalization()(inputs)
+    x = layers.MultiHeadAttention(num_heads=num_heads, key_dim=head_size, dropout=dropout)(x, x)
+    x = layers.Dropout(dropout)(x)
+    res = x + inputs
+
+    # Feed Forward Part
+    x = LayerNormalization(epsilon=1e-6)(res)
+    x = layers.Conv1D(filters=ff_dim, kernel_size=1, activation='relu')(x)
+    x = layers.Dropout(dropout)(x)
+    x = layers.Conv1D(filters=inputs.shape[-1], kernel_size=1)(x)
+    return x + res 
+
+def build_tran(X, num_classes, set, opt='SGD', loss='mean_squared_error'):
+    print("Input Shape: ", X.shape)
+    config_dic = loadDic('Transformer')
+    inputs = layers.Input(shape=X[0].shape)
+    x = inputs
+    for _ in range(config_dic[set]['num_attn+layers']):
+        x = transformer_encoder(x, config_dic[set]['head_size'], config_dic[set]['num_heads'], config_dic[set]['ff_dim'], config_dic[set]['dropout'])
+    x = layers.Dense(128, activation='relu')(x)
+    x = layers.Flatten()(x)
+    outputs = layers.Dense(num_classes, activation='softmax')(x)
+    model = Model(inputs, outputs)
+    model.compile(optimizer=opt, loss=loss, metrics=[met.CategoricalAccuracy()])
+    model.summary()
+    return model
+
 if __name__ == '__main__':
     print('### Generate Dummy Data ###')
     X = np.reshape(np.array([generate_pattern_data_as_array(length=150) for _ in range(100)]), (100,1,150))
+    y = np.array([randint(0,1) for _ in range(100)])
     config_dic = loadDic('CNN')
     print(X.shape)
 
     print('### Build and Viosualize CNN')
-    model = Sequential()
-    model.add(layers.InputLayer(input_shape=X[0].shape))
-    model.add(layers.Reshape((1,150)))
-    model.add(layers.Conv1D(filters=64, activation='relu', padding='valid', kernel_size=(3), data_format='channels_first'))
-    model.add(layers.Conv1D(filters=64, activation='relu', padding='valid', kernel_size=(3), data_format='channels_first'))
-    model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
-    visualkeras.layered_view(model, to_file='imgs/model_layers/test.png', min_xy=10, min_z=10, scale_xy=100, scale_z=100, one_dim_orientation='x')
+    model = build_cnn(X, 2, 'ss1')
+
+    model.summary()
+
+    color_map = defaultdict(dict)
+    color_map[layers.Conv1D]['fill'] = 'orange'
+    color_map[BatchNormalization]['fill'] = 'gray'
+    color_map[layers.Dropout]['fill'] = 'pink'
+    color_map[layers.MaxPooling1D]['fill'] = 'red'
+    color_map[layers.Dense]['fill'] = 'green'
+    color_map[layers.Flatten]['fill'] = 'teal'
+    color_map[layers.LSTM]['fill'] = 'blue'
+    color_map[layers.GlobalAveragePooling1D]['fill'] = 'purple'
+    color_map[layers.MultiHeadAttention]['fill'] = 'yellow'
+
+    ignore = [BatchNormalization, layers.Input, layers.InputLayer, layers.core.TFOpLambda, ModuleWrapper]
+
+    system_fonts = matplotlib.font_manager.findSystemFonts(fontpaths=None, fontext='ttf')
+    print(system_fonts)
+    font = ImageFont.truetype('LiberationSerif-Regular.ttf', size = 64)   
 
     print('###Save visualization of CNN###')
-    #visualkeras.layered_view(model, to_file='imgs/model_layers/test.png')
+    visualkeras.layered_view(model, to_file='imgs/model_layers/CNN_visualkeras.png',  legend=True, scale_xy=.5, scale_z=1, color_map=color_map, type_ignore=ignore)
+
+    print('### Build and Viosualize LSTM')
+    model = build_lstm(X, 2, 'ss1')
+    print(model.layers)
+    print('###Save visualization of LSTM###')
+    visualkeras.layered_view(model, to_file='imgs/model_layers/LSTM_visualkeras.png',  legend=True, scale_xy=.5, scale_z=1, color_map=color_map, type_ignore=ignore)
+
+    print('### Build and Viosualize Transformer')
+    model = build_tran(X, 2, 'ss1')
+
+    print('###Save visualization of Transformer###')
+    visualkeras.layered_view(model, to_file='imgs/model_layers/Transformer_visualkeras.png',  legend=True, scale_xy=.5, scale_z=1, color_map=color_map, type_ignore=ignore)
